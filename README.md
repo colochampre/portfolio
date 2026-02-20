3788f1e1d1c1c18ba3bc9a2b57c76c9e60bc6943
Colo Champre <colochampre3@gmail.com>
2026-02-19 22:33:04 -0300

    configuracion de Express

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portfolio - Juan Martin Champredonde
+
+Desarrollado con Node.js, Express y Pug
+
+## Desarrollo
+### Requisitos
+- Node.js
+- npm
+
+### Instalación
+```bash
+# Clonar el repositorio
+git clone https://github.com/colochampre/portfolio.git
+cd portfolio
+# Instalar dependencias
+npm install
+```
---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+//import taskController from "./controllers/taskController.js";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
